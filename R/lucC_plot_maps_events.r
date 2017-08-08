356df35c073bb04317c0ba2a4c90ef6fd0e8bef0
ammaciel <adelsud6@gmail.com>
2017-08-08 18:12:15 -0300

    Add new type of plot, freplot, and new parameters to plots functions. Update 08 August 2017.

--- R/lucC_plot_maps_events.r
+++ R/lucC_plot_maps_events.r
@@ -74,7 +74,7 @@
 #' time_ex1 <- lucC_interval("2002-01-01", "2014-01-01")
 #' 
 #' # apply predicate occur
-#' ts_occur1 <- lucC_predicate_occur(geo_objects = input_tb_raw_json, 
+#' ts_occur1 <- lucC_predicate_holds(geo_objects = input_tb_raw_json, 
 #' object_properties = "Forest", event_time_intervals = time_ex1)
 #' ts_occur1
 #' 
@@ -277,7 +277,7 @@ lucC_plot_maps_events <- function(data_tb = NULL, EPSG_WGS84 = TRUE, custom_pale
 #' time_ex1 <- lucC_interval("2002-01-01", "2014-01-01")
 #' 
 #' # apply predicate occur
-#' ts_occur1 <- lucC_predicate_occur(geo_objects = input_tb_raw_json, 
+#' ts_occur1 <- lucC_predicate_holds(geo_objects = input_tb_raw_json, 
 #' object_properties = "Forest", event_time_intervals = time_ex1)
 #' ts_occur1
 #' 
@@ -330,7 +330,7 @@ lucC_plot_sequence_events <- function(data_tb = NULL, custom_palette = FALSE, RG
     my_palette = scales::hue_pal()(colour_count)
   } 
   
-  original_leg_lab <- levels(droplevels(mapSeq$label))
+  original_leg_lab <- unique(mapSeq$label)
   cat("Original legend labels: \n", original_leg_lab, "\n")
   
   # insert own legend text
@@ -392,7 +392,8 @@ lucC_plot_sequence_events <- function(data_tb = NULL, custom_palette = FALSE, RG
 #' 
 #' @usage lucC_plot_barplot_events (data_tb = NULL, 
 #' custom_palette = FALSE, RGB_color = NULL, pixel_resolution = 250, 
-#' relabel = FALSE, original_labels = NULL, new_labels = NULL)
+#' relabel = FALSE, original_labels = NULL, new_labels = NULL,
+#' side_by_side = FALSE)
 #' 
 #' @param data_tb          Tibble. A tibble with values longitude and latitude and other values
 #' @param custom_palette   Boolean. A TRUE or FALSE value. If TRUE, user will provide its own color palette setting! Default is FALSE
@@ -401,6 +402,7 @@ lucC_plot_sequence_events <- function(data_tb = NULL, custom_palette = FALSE, RG
 #' @param relabel          Boolean. A TRUE or FALSE value. If TRUE, user will provide its own legend text setting! Default is FALSE
 #' @param original_labels  Character. A vector with original labels from legend text, for example, c("Forest","Pasture").
 #' @param new_labels       Character. A vector with new labels to legend text, for example, c("Mature_Forest","Pasture1").
+#' @param side_by_side     Boolean. Make bar of a barplot a side-by-side plot. Default is FALSE.
 #' 
 #' @keywords datasets
 #' @return Plot a barplot in Y axis in square kilometers (Area km^2) = (Number of pixel *(pixel_resolution*pixel_resolution))/(1000*1000)
@@ -433,7 +435,7 @@ lucC_plot_sequence_events <- function(data_tb = NULL, custom_palette = FALSE, RG
 #' time_ex1 <- lucC_interval("2002-01-01", "2014-01-01")
 #' 
 #' # apply predicate occur
-#' ts_occur1 <- lucC_predicate_occur(geo_objects = input_tb_raw_json, 
+#' ts_occur1 <- lucC_predicate_holds(geo_objects = input_tb_raw_json, 
 #' object_properties = "Forest", event_time_intervals = time_ex1)
 #' ts_occur1
 #' 
@@ -449,7 +451,7 @@ lucC_plot_sequence_events <- function(data_tb = NULL, custom_palette = FALSE, RG
 #'}
 #'
 
-lucC_plot_barplot_events <- function(data_tb = NULL, custom_palette = FALSE, RGB_color = NULL, pixel_resolution = 250, relabel = FALSE, original_labels = NULL, new_labels = NULL){ 
+lucC_plot_barplot_events <- function(data_tb = NULL, custom_palette = FALSE, RGB_color = NULL, pixel_resolution = 250, relabel = FALSE, original_labels = NULL, new_labels = NULL, side_by_side = FALSE){ 
   
   # Ensure if parameters exists
   ensurer::ensure_that(data_tb, !is.null(data_tb), 
@@ -498,8 +500,15 @@ lucC_plot_barplot_events <- function(data_tb = NULL, custom_palette = FALSE, RGB
     my_new_labels = unique(mapBar$Var2)
   } 
   
+  # make side-by-side bar plot
+  if (side_by_side == TRUE){
+    bar_position = "dodge"
+  } else { 
+    bar_position = "fill"
+  }
+    
   g <- ggplot2::ggplot(mapBar,aes(x=mapBar$Var1, y=(mapBar$Freq*(pixel_resolution*pixel_resolution))/(1000*1000), fill=mapBar$Var2))+
-    geom_bar(width = 0.7, stat="identity")+ #, position=position_dodge())+
+    geom_bar(width = 0.7, stat="identity", position = bar_position)+
     theme_bw()+
     ylab(expression(paste("Area ",km^{2}," = ((pixels number x pixel ", resolution^{2},")/",1000^{2},")")))+
     xlab("Years")+
@@ -512,6 +521,141 @@ lucC_plot_barplot_events <- function(data_tb = NULL, custom_palette = FALSE, RGB
   print(g)
   
 }
+
+
+
+#' @title Plot Frequency Polygon with lucC Events
+#' @name lucC_plot_freqplot_events
+#' @aliases lucC_plot_freqplot_events
+#' @author Adeline M. Maciel
+#' @docType data
+#' 
+#' @description Plot frequency polygon over time 
+#' 
+#' @usage lucC_plot_freqplot_events (data_tb = NULL, 
+#' custom_palette = FALSE, RGB_color = NULL, pixel_resolution = 250, 
+#' relabel = FALSE, original_labels = NULL, new_labels = NULL)
+#' 
+#' @param data_tb          Tibble. A tibble with values longitude and latitude and other values
+#' @param custom_palette   Boolean. A TRUE or FALSE value. If TRUE, user will provide its own color palette setting! Default is FALSE
+#' @param RGB_color        Character. A vector with color names to map legend, for example, c("Green","Blue"). Default is setting scale_colour_hue
+#' @param pixel_resolution Numeric. Is a spatial resolution of the pixel. Default is 250 meters considering MODIS 250 m. See more at \url{https://modis.gsfc.nasa.gov/about/specifications.php}.
+#' @param relabel          Boolean. A TRUE or FALSE value. If TRUE, user will provide its own legend text setting! Default is FALSE
+#' @param original_labels  Character. A vector with original labels from legend text, for example, c("Forest","Pasture").
+#' @param new_labels       Character. A vector with new labels to legend text, for example, c("Mature_Forest","Pasture1").
+#' 
+#' @keywords datasets
+#' @return Plot a frequency polygon in Y axis in square kilometers (Area km^2) = (Number of pixel *(pixel_resolution*pixel_resolution))/(1000*1000)
+#' @import ggplot2
+#' @importFrom ensurer ensure_that 
+#' @importFrom lubridate year 
+#' @importFrom scales hue_pal
+#' @export
+#'
+#' @examples \dontrun{
+#' 
+#' library(lucC)
+#' 
+#' lucC_starting_point()
+#' 
+#' # open a JSON file example
+#' file_json = "./inst/extdata/patterns/example_TWDTW.json"
+#' 
+#' # open file JSON
+#' input_tb_raw_json <- file_json %>%
+#'   lucC_fromJSON()
+#' input_tb_raw_json
+#' 
+#' # plot maps input data
+#' lucC_plot_freqplot_events(input_tb_raw_json, EPSG_WGS84 = TRUE, 
+#' custom_palette = TRUE, RGB_color = c("#FFB266", "#1b791f", 
+#' "#929e6e", "#f5e7a1"))
+#' 
+#' # define interval
+#' time_ex1 <- lucC_interval("2002-01-01", "2014-01-01")
+#' 
+#' # apply predicate occur
+#' ts_occur1 <- lucC_predicate_holds(geo_objects = input_tb_raw_json, 
+#' object_properties = "Forest", event_time_intervals = time_ex1)
+#' ts_occur1
+#' 
+#' # events over input map
+#' lucC_plot_maps_events(ts_occur1, EPSG_WGS84 = TRUE, 
+#' custom_palette = TRUE, RGB_color = c("#FFB266", "#1b791f", 
+#' "#929e6e", "#f5e7a1"), shape_point = 0, colour_point = "black", 
+#' size_point = 2.3)
+#' 
+#' lucC_plot_freqplot_events(ts_occur1, custom_palette = TRUE, 
+#' RGB_color = "#929e6e", pixel_resolution = 250) 
+#' 
+#'}
+#'
+
+lucC_plot_freqplot_events <- function(data_tb = NULL, custom_palette = FALSE, RGB_color = NULL, pixel_resolution = 250, relabel = FALSE, original_labels = NULL, new_labels = NULL){ 
+  
+  # Ensure if parameters exists
+  ensurer::ensure_that(data_tb, !is.null(data_tb), 
+                       err_desc = "data_tb tibble, file must be defined!\nThis data can be obtained using lucC_plot_maps_events().")
+  ensurer::ensure_that(custom_palette, !is.null(custom_palette), 
+                       err_desc = "custom_palette must be defined, if wants use its own color palette setting! Default is FALSE")
+  ensurer::ensure_that(pixel_resolution, !is.null(pixel_resolution), 
+                       err_desc = "pixel_resolution must be defined! Default is 250 meters on basis of MODIS image")  
+  
+  input_data <- data_tb
+  input_data <- input_data[order(input_data$index),] # order by index
+  
+  #mapFreq <- data.frame(table(input_data$w, input_data$z))
+  mapFreq <- data.frame(table(lubridate::year(input_data$end_date), input_data$label))
+  
+  # insert own colors palette
+  if(custom_palette == TRUE){
+    if(is.null(RGB_color) | length(RGB_color) != length(unique(mapFreq$Var2))){
+      cat("\nIf custom_palette = TRUE, a RGB_color vector with colors must be defined!")
+      cat("\nProvide a list of colors with the same length of the number of legend!\n") 
+    } else {
+      my_palette = RGB_color  
+    }
+  } else {
+    # more colors
+    colour_count = length(unique(mapFreq$Var2))
+    my_palette = scales::hue_pal()(colour_count)
+  } 
+  
+  original_leg_lab <- levels(droplevels(mapFreq$Var2))
+  cat("Original legend labels: \n", original_leg_lab, "\n")
+  
+  # insert own legend text
+  if(relabel == TRUE){
+    if(is.null(original_labels) | length(new_labels) != length(unique(mapFreq$Var2)) | 
+       all(original_labels %in% original_leg_lab) == FALSE){
+      cat("\nIf relabel = TRUE, a vector with original labels must be defined!")
+      cat("\nProvide a list of original labels and new labels with the same length of the legend!\n") 
+    } else {
+      my_original_label = original_labels
+      my_new_labels = new_labels
+    }
+  } else {
+    # my legend text
+    my_original_label = unique(mapFreq$Var2)
+    my_new_labels = unique(mapFreq$Var2)
+  } 
+  
+  g <- ggplot2::ggplot(mapFreq,aes(x=mapFreq$Var1, y=(mapFreq$Freq*(pixel_resolution*pixel_resolution))/(1000*1000), group = mapFreq$Var2, color = mapFreq$Var2))+
+    geom_freqpoly(stat = "identity", size = 1)+
+    geom_point( size = 2, shape = 16) +
+    theme_bw()+
+    ylab(expression(paste("Area ",km^{2}," = ((pixels number x pixel ", resolution^{2},")/",1000^{2},")")))+
+    xlab("Years")+
+    scale_color_manual(name="Legend:", values = my_palette, breaks = my_original_label, labels = my_new_labels) +
+    #scale_fill_grey(name = "Legend:", start = 0, end = 0.8) +
+    theme(legend.position = "bottom", 
+          #legend.text=element_text(size=10), 
+          legend.key = element_blank())
+  
+  print(g)
+  
+}
+
 
 
 
